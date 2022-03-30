a56b4531b9efd635fd6ccd09f64b424070f8054a
nplak <n.plaksin8@gmail.com>
2022-03-30 03:00:53 +0300

    [fix] multiple plots

--- utils.py
+++ utils.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 
 
 def save_images(model, dataset, dirpath, idxs, iteration, device):
-    fig, axs = plt.subplots(1, 3)
-    for idx in idxs:
+    fig, axs = plt.subplots(len(idxs), 3)
+    for i, idx in enumerate(idxs):
         plot_name = f"it-{iteration}_idx{idx}.png"
         x, y, c = dataset[idx]
         pred = model(x[None, ...].to(device))
-        axs[0].imshow(x.numpy()[0])
-        axs[1].imshow(y.numpy()[0])
-        axs[2].imshow(pred.detach().cpu().numpy()[0, 0])
-        fig.savefig(os.path.join(dirpath, plot_name))
-        plt.close()
+        axs[i][0].imshow(x.numpy()[0])
+        axs[i][1].imshow(y.numpy()[0])
+        axs[i][2].imshow(pred.detach().cpu().numpy()[0, 0])
+    fig.savefig(os.path.join(dirpath, plot_name))
+    plt.close()
